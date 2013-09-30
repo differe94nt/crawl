3422b07617ab198e564967f161e90c8e95f45dc4
mhshih <d00142002@ntu.edu.tw>
2013-09-30 13:34:40 +0800

    Update crawl.py

--- crawl.py
+++ crawl.py
@@ -1,8 +1,8 @@
 tocrawl=['http://msn.com']
 crawled=[]
 while tocrawl:
-    url=tocrawl.pop()
-    page=get_page(url)
-    tocrawl+=get_links(page)
-    crawled+=url
+    link=tocrawl.pop()
+    page=get_page(link)
+    tocrawl.append(get_all_links(page))
+    crawled.append(link)
 print crawled
